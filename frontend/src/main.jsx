917fa7d13e6852f225fa9b868aeea43ac6253e27
Simone Francia <francia.brtndr@MacBookAir.homenet.telecomitalia.it>
2025-11-18 16:44:01 +0100

    debug: log stripe public key in browser

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 console.log("VITE_API_BASE_URL =", import.meta.env.VITE_API_BASE_URL);
+console.log("Stripe key =", import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 import { createRoot } from "react-dom/client";
 import Home from "./Home.jsx";
 import "./styles/global.css";
